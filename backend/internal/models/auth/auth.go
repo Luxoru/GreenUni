fbc26d1a37d522ed114afdc7af6995bb495133db
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:18 +0100

    Basic models for json response

---
+++ backend/internal/models/auth/auth.go
@@ -0,0 +1,15 @@
+package auth
+
+import "backend/internal/models"
+
+type LoginStatus struct {
+	LoginAllowed bool        `json:"success"`
+	Message      string      `json:"message"`
+	Info         interface{} `json:"info,omitempty"`
+}
+
+type SignupStatus struct {
+	Model   *models.UserInfoModel `json:"user"`
+	Message string                `json:"message"`
+	Success bool                  `json:"success"`
+}
