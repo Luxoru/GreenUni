bf2fa78053179cc3a89ec51520ea2d632d3f0673
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:20 +0100

    testt

---
+++ backend/routes/pathapi/v1/root/route.go
@@ -0,0 +1,41 @@
+package root
+
+import (
+	"backend/internal/db/adapters/mysql"
+	"backend/routes/pathapi"
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+type Path struct {
+	router chi.Router
+}
+
+type MessyData struct {
+	ID   int
+	Name string
+}
+
+func (path *Path) SetupComponents(_ *mysql.Repository) chi.Router {
+	r := chi.NewRouter()
+	r.Get("/", GetDefault)
+	path.router = r
+	return r
+}
+
+func GetDefault(w http.ResponseWriter, r *http.Request) {
+	data := MessyData{
+		ID:   1,
+		Name: "Test",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		return
+	}
+}
+
+func Route() pathapi.PathComponent {
+	return &Path{}
+}
