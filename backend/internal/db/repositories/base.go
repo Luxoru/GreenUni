c94b77d6b562248969067d61d9fd2b11548051c4
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:16 +0100

    Generic repository to be implemented by all repositories

---
+++ backend/internal/db/repositories/base.go
@@ -0,0 +1,60 @@
+package repositories
+
+import (
+	"backend/internal/db/adapters/mysql"
+	"strings"
+)
+
+//TODO: implement caching layer
+//TODO: make async
+
+// SQLRepository Interface for repositories that require SQL table creation queries
+type SQLRepository interface {
+	CreateTablesQuery() *[]string
+	CreateIndexesQuery() *[]string
+}
+
+// BaseRepository Struct that holds a reference to a MySQL Repository
+type BaseRepository struct {
+	Repository *mysql.Repository
+}
+
+// InitRepository Initializes the repository, runs SQL queries to create tables
+func InitRepository(repo SQLRepository, container *mysql.Repository) (*BaseRepository, error) {
+
+	repository := container
+
+	base := &BaseRepository{
+		Repository: repository,
+	}
+
+	createTableQueries := *repo.CreateTablesQuery()
+
+	// Execute SQL query for each create table query
+	for _, query := range createTableQueries {
+		rows, err := repository.ExecuteQuery(query, make([]mysql.Column, 0), mysql.QueryOptions{})
+		if err != nil {
+			return nil, err
+		}
+		rows.Close()
+	}
+
+	//Manage indexes
+
+	createTableIndexes := *repo.CreateIndexesQuery()
+
+	for _, query := range createTableIndexes {
+		_, err := repository.ExecuteQuery(query, nil, mysql.QueryOptions{})
+		if err != nil {
+
+			if strings.Contains(err.Error(), "Duplicate key name") {
+				// The index already exists — can ignore error
+				continue
+			}
+			return nil, err
+
+		}
+	}
+
+	return base, nil
+}
