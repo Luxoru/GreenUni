03d19fbab221611ad61d63a1a69651a3036f0a26
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:22 +0100

    Moved alot of logic to service

--- backend/routes/pathapi/v1/user/user.go
+++ backend/routes/pathapi/v1/user/user.go
@@ -1,17 +1,13 @@
 package user
 
 import (
-	"backend/api"
 	"backend/internal/db/adapters/mysql"
 	"backend/internal/db/repositories"
 	"backend/internal/models"
 	"backend/internal/service/user"
+	response "backend/internal/utils/http"
 	"backend/routes/pathapi"
-	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi"
-	"github.com/google/uuid"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -44,69 +40,17 @@ func (path *Path) SetupComponents(sqlRepository *mysql.Repository) chi.Router {
 func (path *Path) GetUser(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	userID := query.Get("userID")
+	username := query.Get("username")
 
-	path.service.GetUser()
-
-	var user *models.RawUserRow
-
-	if userID != "" {
-		parsedUUID, err := uuid.Parse(userID)
-		if err != nil {
-			err = fmt.Errorf("uuid couldn't be parsed")
-			api.RequestErrorhandler(w, err)
-			return
-		}
-		user, err = path.service.GetRawUserByID(parsedUUID)
-		if err != nil {
-			log.Errorf("error occured: %s", err)
-			err = fmt.Errorf("error occured fetching user")
-			api.RequestErrorhandler(w, err)
-			return
-		}
-		if user == nil {
-			err = fmt.Errorf("user with id %s doesnt exist", parsedUUID)
-			api.RequestErrorhandler(w, err)
-			return
-		}
-
-	} else {
-		username := query.Get("username")
-		var err error
-		if username == "" {
-			err = fmt.Errorf("invalid request. Username/UserID not defined")
-			api.RequestErrorhandler(w, err)
-			return
-		}
-
-		user, err = path.service.GetRawUserByName(username)
-		if err != nil {
-			log.Errorf("error occured: %s", err)
-			err = fmt.Errorf("error occured fetching user")
-			api.RequestErrorhandler(w, err)
-			return
-		}
-		if user == nil {
-			err = fmt.Errorf("user with name %s doesnt exist", username)
-			api.RequestErrorhandler(w, err)
-			return
-		}
-
+	model := models.GetUserRequest{
+		UserUUID: userID,
+		Username: username,
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	getUser := path.service.GetUser(&model)
 
-	userInfo := &models.UserInfoModel{
-		UUID:     user.UUID,
-		Username: user.Username,
-	}
+	response.WriteJson(w, getUser)
 
-	if userInfo == nil {
-		err := fmt.Errorf("ended up nill")
-		api.RequestErrorhandler(w, err)
-		return
-	}
-
-	json.NewEncoder(w).Encode(userInfo)
 }
 
 func Route() pathapi.PathComponent {
