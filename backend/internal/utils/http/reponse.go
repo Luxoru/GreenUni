c05bf9ba3301b2a991b05007eab895cc9298e0bf
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:21 +0100

    Added success and error formatting

--- backend/internal/utils/http/reponse.go
+++ backend/internal/utils/http/reponse.go
@@ -9,3 +9,17 @@ func WriteJson(w http.ResponseWriter, obj interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(obj)
 }
+
+func SuccessResponse(data interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"success": true,
+		"data":    data,
+	}
+}
+
+func ErrorResponse(message string) map[string]interface{} {
+	return map[string]interface{}{
+		"success": false,
+		"message": message,
+	}
+}
