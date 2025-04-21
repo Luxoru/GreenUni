4e30aed0bbb8b879bec226efd06ef585f40846d0
Luxoru <luxoru.dev@gmail.com>
2025-04-21 04:09:17 +0100

    Added all api endpoints

---
+++ backend/internal/handlers/api.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"backend/internal/db/adapters/mysql"
+	"backend/routes/pathapi"
+	"backend/routes/pathapi/v1/auth"
+	"backend/routes/pathapi/v1/opportunities"
+	"backend/routes/pathapi/v1/root"
+	"backend/routes/pathapi/v1/user"
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+// routeRegistry maps API versions to their respective routes and handlers
+var routeRegistry = map[string]map[string]func() pathapi.PathComponent{
+	"v1": {
+		"/":              root.Route,
+		"/user":          user.Route,
+		"/auth/signup":   auth.SignupRoute,
+		"/auth/login":    auth.LoginRoute,
+		"/opportunities": opportunities.OpportunityRoute,
+	},
+}
+
+// Handler sets up the routing for the application using the Chi router
+func Handler(r *chi.Mux, sqlRepository *mysql.Repository) {
+	r.Use(chimiddle.StripSlashes)
+	for version, routes := range routeRegistry {
+		r.Route("/api/"+version, func(v chi.Router) {
+			for path, routeFunc := range routes {
+				v.Mount(path, routeFunc().SetupComponents(sqlRepository))
+			}
+		})
+	}
+}
