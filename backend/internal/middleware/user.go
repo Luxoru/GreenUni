5da5094de4624cdda29474659e790e0c179e0b74
Luxoru <luxoru.dev@gmail.com>
2025-04-28 00:02:24 +0100

    Added some checks

--- backend/internal/middleware/user.go
+++ backend/internal/middleware/user.go
@@ -1,5 +1,29 @@
 package middleware
 
-func S() {
+import (
+	"backend/internal/models"
+	"backend/internal/security"
+	"net/http"
+)
 
+func CheckIfAdminUser(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+
+		userInfo, err := security.ExtractUserInfoFromJWT(request)
+		if err != nil {
+			http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if userInfo == nil {
+			http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if userInfo.Role != models.Admin {
+			http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		handler.ServeHTTP(writer, request)
+	})
 }
