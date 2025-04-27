fdf3492aac9da5827d0371e346b5f2b889a9dc32
Luxoru <luxoru.dev@gmail.com>
2025-04-28 00:03:20 +0100

    Update opportunity stuff

--- backend/internal/service/opportunity/opportunityService.go
+++ backend/internal/service/opportunity/opportunityService.go
@@ -93,6 +93,90 @@ func writeStatus(model *models.OpportunityModel, message string, success bool) *
 	}
 }
 
+func (service *OpportunityService) UpdateOpportunity(request models.CreateOpportunityRequest) *response.Response {
+	if request.Title == "" || request.Type == "" || request.Description == "" || request.Location == "" || request.AuthorUUID == "" || request.Points == 0 || request.UUID == "" {
+		return response.ErrorResponse("Invalid request format")
+	}
+
+	opportunityUUID, err := uuid.Parse(request.UUID)
+	if err != nil {
+		return response.ErrorResponse("Unable to parse UUID")
+	}
+
+	model := &models.OpportunityModel{
+		UUID:            opportunityUUID,
+		Title:           request.Title,
+		Description:     request.Description,
+		Points:          request.Points,
+		Location:        request.Location,
+		OpportunityType: request.Type,
+	}
+
+	var modelTags []models.TagModel
+
+	for _, tag := range request.Tags {
+		model := models.TagModel{
+			TagName: tag,
+		}
+
+		modelTags = append(modelTags, model)
+	}
+	var media []models.MediaModel
+
+	types := request.MediaTypes
+	urls := request.MediaURLs
+
+	for i := range types {
+
+		parseMediaType, err := models.ParseMediaType(types[i])
+		if err != nil {
+			log.Error(err)
+			return response.ErrorResponse("Internal error occured whilst processing media type")
+		}
+
+		media = append(media, models.MediaModel{
+			Type: parseMediaType,
+			URL:  urls[i],
+		})
+	}
+
+	model.Tags = &modelTags
+	model.Media = &media
+
+	err = service.repo.UpdateOpportunity(model)
+	if err != nil {
+		return response.ErrorResponse("Internal error occured")
+	}
+
+	return response.SuccessResponse(model, "")
+}
+
+func (service *OpportunityService) UpdateStatus(opportunityID string, status string) *response.Response {
+
+	if opportunityID == "" {
+		return response.ErrorResponse("Opportunity UUID not provided")
+	}
+
+	opportunityUUID, err := uuid.Parse(opportunityID)
+
+	if err != nil {
+		return response.ErrorResponse("Unable to parse UUID")
+	}
+
+	opportunityStatus, err := strconv.ParseBool(status)
+
+	if err != nil {
+		return response.ErrorResponse("Unable to parse status")
+	}
+
+	err = service.repo.UpdateOpportunityStatus(opportunityUUID, opportunityStatus)
+	if err != nil {
+		return response.ErrorResponse("Internal error occurred")
+	}
+
+	return response.SuccessResponse(nil, "")
+}
+
 // GetOpportunity retrieves an opportunity by its UUID.
 func (service *OpportunityService) GetOpportunity(opportunityUUID uuid.UUID) (*models.OpportunityModel, error) {
 	opportunity, err := service.repo.GetOpportunity(&opportunityUUID)
@@ -104,6 +188,25 @@ func (service *OpportunityService) GetOpportunity(opportunityUUID uuid.UUID) (*m
 	}
 	model := *opportunity
 	return &model[0], nil
+}
+
+func (service *OpportunityService) GetOpportunitiesByAuthor(authorID string) *response.Response {
+	if authorID == "" {
+		return response.ErrorResponse("Author uuid not provided")
+	}
+
+	authorUUID, err := uuid.Parse(authorID)
+
+	if err != nil {
+		return response.ErrorResponse("Unable to parse uuid")
+	}
+
+	oppportunities, err := service.repo.GetOpportunityByAuthor(&authorUUID)
+	if err != nil {
+		return response.ErrorResponse("Internal error occured")
+	}
+
+	return response.SuccessResponse(oppportunities, "")
 }
 
 // DeleteOpportunity deletes an opportunity by its UUID.
@@ -132,6 +235,46 @@ func (service *OpportunityService) GetOpportunitiesFrom(from string, limit strin
 	}
 
 	return service.repo.GetOpportunitiesFrom(fromInt, limitInt, userUUID)
+}
+
+func (service *OpportunityService) GetOpportunityByLikes(opportunityID string, from string, limit string) *response.Response {
+
+	if opportunityID == "" || from == "" || limit == "" {
+		return response.ErrorResponse("must be provided")
+	}
+
+	opportunityUUID, err := uuid.Parse(opportunityID)
+
+	if err != nil {
+		return response.ErrorResponse("Unable to parse Opportunity UUID")
+	}
+
+	fromInt, err := strconv.ParseInt(from, 10, 64)
+	if err != nil {
+		return response.ErrorResponse("from is not an integer")
+	}
+
+	limitInt, err := strconv.ParseInt(limit, 10, 64)
+	if err != nil {
+		return response.ErrorResponse("limit is not an integer")
+	}
+
+	likes, lastRow, err := service.repo.GetOpportunityByLikes(opportunityUUID, fromInt, limitInt)
+	if err != nil {
+		log.Error(err)
+		return response.ErrorResponse("Internal error occured")
+	}
+
+	if likes == nil {
+		return response.ErrorResponse("No likes for given opportunity")
+	}
+
+	model := struct {
+		Likes     *[]*models.StudentInfoModel `json:"likes"`
+		LastIndex int64                       `json:"lastIndex"`
+	}{likes, lastRow}
+
+	return response.SuccessResponse(model, "")
 }
 
 func (service *OpportunityService) LikeOpportunity(userID, postID string) *response.Response {
