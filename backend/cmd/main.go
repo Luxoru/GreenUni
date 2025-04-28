98f81cb5f34fffa0b9a911241f0198b0da008677
Luxoru <luxoru.dev@gmail.com>
2025-04-28 03:45:16 +0100

    Added neo repo

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"backend/internal/db"
 	"backend/internal/db/adapters/mysql"
+	"backend/internal/db/adapters/neo4j"
 	"backend/internal/handlers"
 	"fmt"
 	"github.com/go-chi/chi"
@@ -39,11 +40,31 @@ func main() {
 		Database: &container,
 	}
 
+	neoContainer := neo4j.Container{}
+	neoConfig := neo4j.Configurations{
+		Authentication: &db.URIConfigurations{
+			AuthConfig: db.AuthenticationConfigurations{
+				Username: "neo4j",
+				Password: "password",
+			},
+			URI: "bolt://localhost:7687",
+		},
+	}
+
+	err = neoContainer.Connect(neoConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	neoRepo := neo4j.Repository{
+		Database: &neoContainer,
+	}
+
 	// Setup router
 	router := chi.NewRouter()
 	log.SetReportCaller(true)
 
-	handlers.Handler(router, &repo)
+	handlers.Handler(router, &repo, &neoRepo)
 
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
