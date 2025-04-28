be9c9af24c062d584d1b4a7920e23e70421650d7
Luxoru <luxoru.dev@gmail.com>
2025-04-28 03:50:15 +0100

    Generating random strings

---
+++ backend/internal/utils/string/string.go
@@ -0,0 +1,18 @@
+package stringutils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// GenerateRandomString generates a random string of a given length.
+func GenerateRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var result strings.Builder
+	for i := 0; i < length; i++ {
+		result.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return result.String()
+}
