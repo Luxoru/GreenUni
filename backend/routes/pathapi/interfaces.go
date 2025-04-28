652ff5da5438320a76f1e6db122d34306fe44b2d
Luxoru <luxoru.dev@gmail.com>
2025-04-28 03:48:26 +0100

    Changes for neo repo

--- backend/routes/pathapi/interfaces.go
+++ backend/routes/pathapi/interfaces.go
@@ -2,9 +2,10 @@ package pathapi
 
 import (
 	"backend/internal/db/adapters/mysql"
+	"backend/internal/db/adapters/neo4j"
 	"github.com/go-chi/chi"
 )
 
 type PathComponent interface {
-	SetupComponents(sqlRepository *mysql.Repository) chi.Router
+	SetupComponents(sqlRepository *mysql.Repository, repository *neo4j.Repository) chi.Router
 }
